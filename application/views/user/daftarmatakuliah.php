d811a5ce198a74f9c885efd065a6d0408ddd762e
DESKTOP-0US7OGN\Acer <kristantowisnu2312@gmail.com>
2019-12-12 10:59:30 +0800

    Tambah Tombol kembali dashboard di homepage

--- application/views/user/daftarmatakuliah.php
+++ application/views/user/daftarmatakuliah.php
@@ -9,6 +9,9 @@
     <div class="card-header py-3">
       <div class="d-sm-flex align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-primary">Daftar Matakuliah</h6>
+        <?php if ($user['name'] != null) { ?>
+            <a href="<?= base_url('admin'); ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Ke Halaman Dashboard</a>
+        <?php  } ?>
       </div>
     </div>
     <div class="card-body">
